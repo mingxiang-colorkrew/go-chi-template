f5d51640b5872b144584a96fe60e39ea889a0520
Ming Xiang Chan <mingxiangc.isao@gmail.com>
2023-03-25 11:27:41 +0900

    setup: add golang dotenvs

--- config/app.go
+++ config/app.go
@@ -3,16 +3,17 @@ package config
 import (
 	"measure/webserver/router"
 	"net/http"
-
+  "log"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+  "github.com/joho/godotenv"
 )
 
 func StartApp() {
+  setupEnv()
+
 	r := chi.NewRouter()
-
 	r = setupMiddleWare(r)
-
 	r = router.Setup(r)
 
 	http.ListenAndServe(":3000", r)
@@ -22,4 +23,11 @@ func setupMiddleWare(r *chi.Mux) *chi.Mux {
 	r.Use(middleware.Logger)
 
 	return r
+}
+
+func setupEnv() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
 }
